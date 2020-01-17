828e0fd8cae34c05ef7ebcb5e5ca2f87a42f30ab
Nipun Chhajed <nipunchhajed@gmail.com>
2020-01-17 14:37:19 -0800

    basic app UI set up

--- quizzy/lib/main.dart
+++ quizzy/lib/main.dart
@@ -1,15 +1,47 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:quizzy/quizcard.dart';
 
 void main() {
   runApp(
     new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Quizzy",
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Quizzy", style: GoogleFonts.lato()),
-        ),
-      ),
+      home: new Quiz(),
     ),
   );
+}
+
+class Quiz extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new QuizState();
+  }
+}
+class QuizState extends State<Quiz> {
+  var appbarColor = Color(0xffEF767A);
+  var bgcolor = Color(0xff456990);
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: bgcolor,
+      appBar: new AppBar(
+        title: new Text("Quizzy", style: GoogleFonts.bungee()),
+        backgroundColor: appbarColor,
+        centerTitle: true,
+      ),
+      body: ListView (
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          quizCard('first quiz', 'second quiz'),
+          quizCard('second quiz', 'trying random stuff'),
+          quizCard('third quiz', 'trying random stuff'),
+          quizCard('fourth quiz', 'trying random stuff'),
+          quizCard('fifth quiz', 'trying random stuff'),
+          quizCard('sixth quiz', 'trying random stuff'),
+          quizCard('seventh quiz', 'trying random stuff')
+        ],
+      ),
+    );
+  }
 }
---
+++ quizzy/lib/quizcard.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quizzy/quizpage.dart';
+import 'package:quizzy/main.dart';
+
+Widget quizCard(String name, String des){
+    const cardcolor = Color(0xff49BEAA);
+    const buttoncolor = Color(0xffEEB868);
+    return Center(
+      child: Card(
+        color: cardcolor,
+        elevation: 20,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.assignment),
+              title: Text(name, style: GoogleFonts.bungee(fontSize: 20, textStyle: TextStyle(color: Colors.white))),
+              subtitle: Text(des, style: GoogleFonts.bungee(fontSize: 15, textStyle: TextStyle(color: Colors.white))),
+            ),
+            ButtonBar(
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Play', style: GoogleFonts.bungee(textStyle: TextStyle(color: Colors.white))),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  color: buttoncolor,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+}
