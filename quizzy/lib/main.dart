e27a0df7c08cd1d3450973dd82cb17e7674bb877
Nipun Chhajed <nipunchhajed@gmail.com>
2020-02-03 00:26:15 -0800

    quizpage updates

--- quizzy/lib/main.dart
+++ quizzy/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:quizzy/quizcard.dart';
+import 'package:quizzy/quizpage.dart';
 
 void main() {
   runApp(
@@ -33,13 +34,14 @@ class QuizState extends State<Quiz> {
       body: ListView (
         padding: EdgeInsets.all(10.0),
         children: <Widget>[
-          quizCard('first quiz', 'second quiz'),
-          quizCard('second quiz', 'trying random stuff'),
-          quizCard('third quiz', 'trying random stuff'),
-          quizCard('fourth quiz', 'trying random stuff'),
-          quizCard('fifth quiz', 'trying random stuff'),
-          quizCard('sixth quiz', 'trying random stuff'),
-          quizCard('seventh quiz', 'trying random stuff')
+          quizPage(7, "try this question for starters", ["Mr. Xu needs to be more productive in life", "Mr. Yum time to help some people", "daaaaaaaaaaaaaaaaaaaang Genius Acchin", "PED TDM BDB CHUNKY CHUNG SKEWER"])
+//          quizCard('first quiz', 'second quiz'),
+//          quizCard('second quiz', 'trying random stuff'),
+//          quizCard('third quiz', 'trying random stuff'),
+//          quizCard('fourth quiz', 'trying random stuff'),
+//          quizCard('fifth quiz', 'trying random stuff'),
+//          quizCard('sixth quiz', 'trying random stuff'),
+//          quizCard('seventh quiz', 'trying random stuff')
         ],
       ),
     );
--- quizzy/lib/quizpage.dart
+++ quizzy/lib/quizpage.dart
@@ -2,5 +2,104 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quizzy/quizpage.dart';
+import 'package:quizzy/main.dart';
 
-Widget 
+const cardcolor = Color(0xff49BEAA);
+const buttoncolor = Color(0xffEEB868);
+
+Widget quizPage(int number, String question, List answers){
+  return Center(
+    child: Column(
+      children: <Widget>[
+        questionCard(number, question),
+        answerCard(answers)
+      ]
+    )
+  );
+}
+
+Widget questionCard(int number, String question) {
+  return Card(
+    color: cardcolor,
+    elevation: 20,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30.0),
+    ),
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.assignment),
+          title: Text("Question $number", style: GoogleFonts.bungee(fontSize: 20, textStyle: TextStyle(color: Colors.white))),
+          subtitle: Text(question, style: GoogleFonts.bungee(fontSize: 15, textStyle: TextStyle(color: Colors.white))),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget answerCard(List answers) {
+  return Card(
+      color: cardcolor,
+      elevation: 20,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+    child: Column(
+      
+      children: <Widget>[
+        SizedBox(
+          width: double.infinity,
+          child: FlatButton(
+            padding: EdgeInsets.all(16.0),
+            child: Text(answers[0], style: GoogleFonts.bungee(textStyle: TextStyle(color: Colors.white))),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: buttoncolor,
+            onPressed: () {},
+          ),
+        ),
+        SizedBox (
+          width: 500,
+          child: FlatButton(
+            padding: EdgeInsets.all(20.0),
+            child: Text(answers[1], style: GoogleFonts.bungee(textStyle: TextStyle(color: Colors.white))),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: buttoncolor,
+            onPressed: () {},
+          ),
+        ),
+        SizedBox (
+          width: double.infinity,
+          child: FlatButton(
+            padding: EdgeInsets.all(16.0),
+            child: Text(answers[2], style: GoogleFonts.bungee(textStyle: TextStyle(color: Colors.white))),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: buttoncolor,
+            onPressed: () {},
+          ),
+        ),
+        SizedBox (
+          width: double.infinity,
+          child: FlatButton(
+            padding: EdgeInsets.all(16.0),
+            child: Text(answers[3], style: GoogleFonts.bungee(textStyle: TextStyle(color: Colors.white))),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: buttoncolor,
+            onPressed: () {},
+          ),
+        )
+      ]
+    )
+  );
+}
