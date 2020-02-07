3c8ef6f74bcfa4757884702ba292a03f939d82d7
zigzag2002 <43194632+zigzag2002@users.noreply.github.com>
2020-02-06 23:11:58 -0800

    Add files via upload
    CreateQuestion is just a placeholder right now and it has a the multiplayerquiz format rn so just change it later

--- quizzy/lib/main.dart
+++ quizzy/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:quizzy/createquestion.dart';
 import 'package:quizzy/multiplayerQuiz.dart';
 import 'package:quizzy/singleplayerQuiz.dart';
 const appbarColor = Color(0xffEF767A);
@@ -9,30 +10,49 @@ void main() {
     new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "QuizGame",
-      home: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: new AppBar(
-              bottom: new TabBar(
+      home: FrontPage(),
+
+    ),
+  );
+}
+
+class FrontPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: new AppBar(
+            bottom: new TabBar(
                 tabs: [
                   Tab(icon: Icon(Icons.person)),
                   Tab(icon: Icon(Icons.group))
                 ]
-              ),
-              title: new Text("Quizzy", style: GoogleFonts.bungee()),
-              backgroundColor: appbarColor,
-              centerTitle: true,
             ),
-            body: TabBarView(
-              children: [
-                new SinglePlayerQuiz(),
-                new MultiPlayerQuiz()
-              ]
-            ),
-          )
-      ),
-    ),
-  );
+            title: new Text("Quizzy", style: GoogleFonts.bungee()),
+            backgroundColor: appbarColor,
+            centerTitle: true,
+          ),
+          body: TabBarView(
+                children: [
+                  new SinglePlayerQuiz(),
+                  new MultiPlayerQuiz(),
+                ]
+          ),
+          floatingActionButton: FloatingActionButton(
+            child:Text("TEST"),
+            backgroundColor: Colors.green,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => new CreateQuestion()),
+              );
+            },
+          ),
+        )
+
+    );
+  }
 }
 
 
